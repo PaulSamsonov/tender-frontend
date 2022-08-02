8f3b824e798d628e0e3e2c2308895e19a56fc4ef
Eli <eli.sakov@hotmail.com>
2022-08-02 07:33:18 -0700

    switch network text

--- app/components/disconnected-warning.tsx
+++ app/components/disconnected-warning.tsx
@@ -66,7 +66,7 @@ export default function Disconnected() {
             className="underline"
             onClick={() => tryConnectingToMetis(provider)}
           >
-            Switch to Metis
+            Switch to Stardust Testnet
           </button>
         </div>
       )}
